cc0efdc9b658b163aa7c7d6e461e5b575d8f5ecb
20181105879 <2605753145@qq.com>
2019-12-18 11:52:32 +0800

    gds
    fdgfdsg

--- kaosi/ViewController.swift
+++ kaosi/ViewController.swift
@@ -9,6 +9,97 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var num1: UITextField!
+    
+    
+    @IBOutlet weak var option: UITextField!
+    
+    
+    @IBOutlet weak var num2: UITextField!
+    
+    
+    @IBOutlet weak var sum: UITextField!
+    
+    @IBOutlet weak var pecture: UIImageView!
+    
+    
+    @IBOutlet weak var processbar: UIView!
+    var count = 0
+    
+    
+    
+    @IBAction func result(_ sender: Any) {
+        count=count+1;
+        var a:Double=Double(num1.text!)!
+        var b:Double=Double(num2.text!)!
+        let op:String=(option.text)!
+        if(op=="+")
+        {
+            sum.text=String(a+b)
+        }
+        else if(op=="-")
+        {
+            sum.text=String(a-b)
+        }
+        else if(op=="*")
+        {
+             sum.text=String(a*b)
+        }
+        else if(op=="/")
+        {
+             sum.text=String(a/b)
+        }
+        
+        start(0)
+        if(count==10)
+        {
+            AlterBtn(0)
+            count=0
+        }
+        
+        
+        
+
+    }
+    
+    @IBAction func rand(_ sender: Any) {
+        
+        var num=Int.random(in: 1...6)
+        
+        pecture.image=UIImage(named: "dice\(num)")
+
+    }
+    
+    
+    @IBAction func start(_ sender: Any) {
+        
+        processbar.frame.size.width=(view.frame.width)-(view.frame.width/10)*CGFloat(count)
+  
+    }
+    
+ 
+
+    
+        
+    @IBAction func AlterBtn(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "提示", message: "弹出来了\n\n你的时间用完了", preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "确定", style: UIAlertAction.Style.default, handler: {action in}))
+        
+        alert.addAction(UIAlertAction(title: "取消", style: UIAlertAction.Style.destructive, handler: {action in}))
+        
+        
+        present (alert,animated: true,completion: nil)
+        
+        //     presentedViewController?.dismiss(animated: true, completion: nil)
+        
+        
+    }
+
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
